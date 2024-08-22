ea9eab1842e1ab5694da305f77a18e1a02d71ed9
ramoncodevale <ramonvalentim88@gmail.com>
2024-08-22 18:59:13 -0300

    feat: add BarberShopItem component

--- app/page.tsx
+++ app/page.tsx
@@ -1,71 +1,85 @@
-import { SearchIcon } from "lucide-react"
-import Header from "./_components/Header"
-import { Button } from "./_components/ui/button"
-import { Input } from "./_components/ui/input"
-import Image from "next/image"
-import { Card, CardContent } from "./_components/ui/card"
-import { Badge } from "./_components/ui/badge"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
+import { SearchIcon } from "lucide-react";
+import Header from "./_components/Header";
+import { Button } from "./_components/ui/button";
+import { Input } from "./_components/ui/input";
+import Image from "next/image";
+import { Card, CardContent } from "./_components/ui/card";
+import { Badge } from "./_components/ui/badge";
+import { Avatar, AvatarImage } from "./_components/ui/avatar";
+import { db } from "./_lib/prisma";
+import BarberShopItem from "./_components/BarberShop-Item";
+
+const Home = async () => {
+  // Chamar meu banco de dados 
+  const barbershops = await db.barbershop.findMany({})
+
+  return (
+    <div>
+      {/* Header */}
+      <Header />
+      <div className="p-5">
+        {/* Texto */}
+        <h2 className="text-xl font-bold">Olá, Ramon!</h2>
+        <p>Quinta-feira 22 de agosto</p>
+
+        {/* Busca */}
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua Busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        {/* Imagem */}
+        <div className="relative mt-6 h-[150px] w-full rounded-xl">
+          <Image
+            alt="Agende com os melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+
+        {/* Agendamento */}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          AGENDAMENTOS
+        </h2>
 
 
-const Home = () => (
-  <div>
-    {/* Header; */}
-    <Header />
-    <div className="p-5">
-      {/* Texto */}
-      <h2 className="text-xl font-bold">Olá, Ramon</h2>
-      <p>Quinta-feira 22 de agosto</p>
+        <Card>
+          <CardContent className="flex justify-between p-0">
+            {/* Esquerda */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3>Corte de Cabelo</h3>
 
-      {/* Busca; */}
-      <div className="mt-6 flex items-center gap-2">
-        <Input placeholder="Faça sua Busca..." />
-        <Button>
-          <SearchIcon />
-        </Button>
-      </div>
-
-
-      {/* Imagem */}
-      <div className="relative mt-6 h-[150px] w-full rounded-xl">
-        <Image
-          alt="Agende com os melhores com FSW Barber"
-          src="/banner-01.png"
-          fill
-          className="rounded-xl object-cover"
-        />
-      </div>
-
-      {/* Agendamento */}
-      <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-        AGENDAMENTOS
-      </h2>
-
-      <Card>
-        <CardContent className="flex justify-between p-0">
-          {/* Esquerda */}
-          <div className="flex flex-col gap-2 py-5 pl-5">
-            <Badge className="w-fit">Confirmado</Badge>
-            <h3>Corte de Cabelo</h3>
-
-            <div className="flex items-center gap-2">
-              <Avatar className="h-6 w-6">
-                <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-              </Avatar>
-              <p className="text-sm">Barbearia FSW</p>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
             </div>
-          </div>
-          {/* Direita */}
-          <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-            <p className="text-sm">Agosto</p>
-            <p className="text-2xl">22</p>
-            <p className="text-sm">17:14</p>
-          </div>
-        </CardContent>
-      </Card>
+            {/* Direita */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">22</p>
+              <p className="text-sm">17:14</p>
+            </div>
+          </CardContent>
+        </Card>
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarberShopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      </div>
     </div>
-  </div>
-)
+  );
+};
 
-export default Home
-
+export default Home;
