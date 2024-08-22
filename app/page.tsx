e680d2e7ac6d8e238ad9d650bc1d6a0084e9e4bb
ramoncodevale <ramonvalentim88@gmail.com>
2024-08-22 15:23:33 -0300

    feat: add Header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,36 @@
-"use client";
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/Header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
-import { useState } from "react";
-import { Button } from "./_components/ui/button";
 
-const Home = () => {
-  const [] = useState()
-  return <Button>Teste</Button>
-}
+const Home = () => (
+  <div>
+    <Header />
+    <div className="p-5">
+      <h2 className="text-xl font-bold">Olá, Ramon</h2>
+      <p>Quinta-feira 22 de agosto</p>
+
+      <div className="mt-6 flex items-center gap-2">
+        <Input placeholder="Faça sua Busca..." />
+        <Button>
+          <SearchIcon />
+        </Button>
+      </div>
+
+      <div className="relative mt-6 h-[150px] w-full rounded-xl">
+        <Image
+          alt="Agende com os melhores com FSW Barber"
+          src="/banner-01.png"
+          fill
+          className="rounded-xl object-cover"
+        />
+      </div>
+
+    </div>
+  </div>
+)
 
 export default Home
 
