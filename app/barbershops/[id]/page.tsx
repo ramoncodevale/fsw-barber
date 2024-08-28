8293232ab0916e3959868760a132590bc8cce71a
ramoncodevale <ramonvalentim88@gmail.com>
2024-08-28 15:01:37 -0300

    feat: ensure barbershop services are shown on barbershop page

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,3 +1,4 @@
+import ServiceItem from "@/app/_components/service-item";
 import { Button } from "@/app/_components/ui/button";
 import { db } from "@/app/_lib/prisma";
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
@@ -17,11 +18,16 @@ const BarberShopPage = async ({ params }) => {
         where: {
             id: params.id,
         },
+        include: {
+            services: true,
+        },
     })
 
     if (!barbershop) {
         return notFound()
     }
+
+    console.log(barbershop.services)
 
     return (
         <div>
@@ -54,21 +60,30 @@ const BarberShopPage = async ({ params }) => {
 
             <div className="border-b border-solid p-5">
                 <h1 className="mb-3 font-bold text-xl">{barbershop.name}</h1>
-              <div className="mb-2 flex items-center gap-2"> 
-                <MapPinIcon className="text-primary" size={18}/>
-                <p className="text-sm">{barbershop?.address}</p>
-              </div>
+                <div className="mb-2 flex items-center gap-2">
+                    <MapPinIcon className="text-primary" size={18} />
+                    <p className="text-sm">{barbershop?.address}</p>
+                </div>
 
-              <div className="flex items-center gap-2"> 
-                <StarIcon className="fill-primary text-primary" size={18} />
-                <p className="text-sm">5,0 (889 avalições)</p>
-              </div>
+                <div className="flex items-center gap-2">
+                    <StarIcon className="fill-primary text-primary" size={18} />
+                    <p className="text-sm">5,0 (889 avalições)</p>
+                </div>
             </div>
 
             {/* DESCRIÇÃO */}
             <div className="spce-y-3 border-b border-solid p-5">
-                <h2 className="font-bold uppercase text-gray-400">Sobre nós</h2>
+                <h2 className="text-xs font-bold uppercase text-gray-400">Sobre nós</h2>
                 <p className="text-justify text-sm">{barbershop?.description}</p>
+            </div>
+
+            <div className="space-y-3 p-5">
+                <h2 className="text-xs font-bold uppercase text-gray-400">Serviços</h2>
+                <div className="space-y-3"> 
+                {barbershop.services.map((service) => (
+                    <ServiceItem key={service.id} service={service} />
+                ))}
+                </div>
             </div>
         </div >
     );
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import { SearchIcon } from "lucide-react";
-import Header from "./_components/Header";
+import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
 import { Input } from "./_components/ui/input";
 import Image from "next/image";
 import { Card, CardContent } from "./_components/ui/card";
 import { db } from "./_lib/prisma";
-import BarberShopItem from "./_components/BarberShop-Item";
+import BarberShopItem from "./_components/barbershop-item";
 import { quickSearchOptions } from "./_constants/search"
 import BookingItem from "./_components/booking-item";
 
