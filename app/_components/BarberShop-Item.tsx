85f6a56d06db627cd11d5265b38f3be17c014db9
ramoncodevale <ramonvalentim88@gmail.com>
2024-08-27 21:30:54 -0300

    feat: add initial barbershop page

--- app/_components/BarberShop-Item.tsx
+++ app/_components/BarberShop-Item.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { Button } from "./ui/button";
 import { Badge } from "./ui/badge";
 import { StarIcon } from "lucide-react";
+import Link from "next/link";
 
 interface BarberShopItemProps {
     barbershop: Barbershop
@@ -33,8 +34,8 @@ const BarberShopItem = ({ barbershop }: BarberShopItemProps) => {
                 <div className="px-1 py-3">
                     <h2 className="truncate font-semibold">{barbershop.name}</h2>
                     <p className="truncate text-sm text-gray-400">{barbershop.address}</p>
-                    <Button variant="secondary" className="mt-3 w-full">
-                        Reservar
+                    <Button variant="secondary" className="mt-3 w-full" asChild>
+                       <Link href={`/barbershops/${barbershop.id}`}>Reservar</Link>
                     </Button>
                 </div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -63,6 +63,8 @@ const Home = async () => {
           />
         </div>
 
+        
+
         {/* Agendamento */}
        <BookingItem />
 
