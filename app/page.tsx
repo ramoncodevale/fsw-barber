ad1d915f9ca4e65ed559fb263c3cc22df86fc013
ramoncodevale <ramonvalentim88@gmail.com>
2024-09-16 19:01:15 -0300

    feat: ensure booking item takes full width home page

--- app/page.tsx
+++ app/page.tsx
@@ -93,7 +93,10 @@ const Home = async () => {
           />
         </div>
 
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+       
+      {confirmedBookings.length >0 && (
+        <>
+          <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Agendamentos
         </h2>
 
@@ -103,6 +106,8 @@ const Home = async () => {
             <BookingItem key={booking.id} booking={booking} />
           ))}
         </div>
+        </>
+      )}
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
