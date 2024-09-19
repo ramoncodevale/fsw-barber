86703f59145e6f9e184f7a78375caa2ec11eee8b
ramoncodevale <ramonvalentim88@gmail.com>
2024-09-19 18:08:35 -0300

    feat: add Header components Desktop

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,27 +1,49 @@
+"use client"
+
 import Image from "next/image"
 import { Card, CardContent } from "./ui/card"
 import { Button } from "./ui/button"
-import { MenuIcon } from "lucide-react"
+import { CalendarIcon, MenuIcon } from "lucide-react"
 import { Sheet, SheetTrigger } from "./ui/sheet"
 import SidebarSheet from "./sidebar-sheet"
 import Link from "next/link"
+import { Avatar, AvatarImage } from "./ui/avatar"
+import { useSession } from "next-auth/react"
 
 const Header = () => {
+  const { data } = useSession()
+
   return (
-    <Card>
+    <Card className="lg:px-32">
       <CardContent className="flex flex-row items-center justify-between p-5">
         <Link href="/">
           <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
         </Link>
 
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button size="icon" variant="outline">
-              <MenuIcon />
-            </Button>
-          </SheetTrigger>
-          <SidebarSheet />
-        </Sheet>
+
+        <div className="lg:hidden">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button size="icon" variant="outline">
+                <MenuIcon />
+              </Button>
+            </SheetTrigger>
+            <SidebarSheet />
+          </Sheet>
+        </div>
+        <ul className="hidden lg:flex items-center gap-10">
+          <li className="flex items-center gap-2">
+            <CalendarIcon size={18} />
+            <Link href="/bookings">
+              Agendamentos</Link>
+          </li>
+          <li className="flex items-center gap-2">
+            <Avatar>
+              <AvatarImage src={data?.user?.image ?? ""} />
+            </Avatar>
+            <p className="font-bold">{data?.user?.name ?? ""}</p>
+          </li>
+        </ul>
       </CardContent>
     </Card>
   )
