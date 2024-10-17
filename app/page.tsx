fad9afc8dd14f62a9dedfba9cadec4ca5f547f6c
ramoncodevale <ramonvalentim88@gmail.com>
2024-10-17 19:08:43 -0300

    fix: aligning the cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,34 @@
-import { Button } from "./_components/ui/button"
-import Image from "next/image"
-import { db } from "./_lib/prisma"
-import BarbershopItem from "./_components/barbershop-item"
-import { quickSearchOptions } from "./_constants/search"
-import BookingItem from "./_components/booking-item"
-import Search from "./_components/search"
-import Link from "next/link"
-import { getServerSession } from "next-auth"
-import { authOptions } from "./_lib/auth"
-import { format } from "date-fns"
-import { ptBR } from "date-fns/locale"
-import { getConfirmedBookings } from "./data/get-confirmed-bookings"
-import Header from "./_components/header"
+import { Button } from "./_components/ui/button";
+import Image from "next/image";
+import { db } from "./_lib/prisma";
+import BarbershopItem from "./_components/barbershop-item";
+import { quickSearchOptions } from "./_constants/search";
+import BookingItem from "./_components/booking-item";
+import Search from "./_components/search";
+import Link from "next/link";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./_lib/auth";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import { getConfirmedBookings } from "./data/get-confirmed-bookings";
+import Header from "./_components/header";
 
 const Home = async () => {
-  const session = await getServerSession(authOptions)
-  const barbershops = await db.barbershop.findMany({})
+  const session = await getServerSession(authOptions);
+  const barbershops = await db.barbershop.findMany({});
   const popularBarbershops = await db.barbershop.findMany({
     orderBy: {
       name: "desc",
     },
-  })
-  const confirmedBookings = await getConfirmedBookings()
+  });
+  const confirmedBookings = await getConfirmedBookings();
 
   return (
     <div>
-      {/* header */}
-      <Header/>
-      <div className="p-5">
-        {/* TEXTO */}
+      {/* Header */}
+      <Header />
+      <div className="p-5 lg:p-5">
+        {/* Texto de Boas-Vindas */}
         <h2 className="text-xl font-bold">
           Olá, {session?.user ? session.user.name : "bem vindo"}!
         </h2>
@@ -42,13 +42,40 @@ const Home = async () => {
           </span>
         </p>
 
-        {/* BUSCA */}
-        <div className="mt-6">
-          <Search />
+       
+        <div className="lg:flex lg:items-center lg:gap-6">
+          {/* Busca */}
+          <div className="lg:w-2/3">
+            <div className="mt-6">
+              <Search />
+            </div>
+
+            
+            <div className="relative mt-6 h-[150px] w-full lg:hidden">
+              <Image
+                alt="Agende nos melhores com FSW Barber"
+                src="/banner.png"
+                fill
+                className="rounded-xl object-cover"
+              />
+            </div>
+          </div>
+
+          {/* Recomendados */}
+          <div className="lg:w-1/3">
+            <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+              Recomendados
+            </h2>
+            <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+              {barbershops.map((barbershop) => (
+                <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+              ))}
+            </div>
+          </div>
         </div>
 
-        {/* BUSCA RÁPIDA */}
-        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+        {/* Busca Rápida */}
+        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden lg:hidden">
           {quickSearchOptions.map((option) => (
             <Button
               className="gap-2"
@@ -69,23 +96,12 @@ const Home = async () => {
           ))}
         </div>
 
-        {/* IMAGEM */}
-        <div className="relative mt-6 h-[150px] w-full">
-          <Image
-            alt="Agende nos melhores com FSW Barber"
-            src="/banner.png"
-            fill
-            className="rounded-xl object-cover"
-          />
-        </div>
-
+        {/* Agendamentos Confirmados */}
         {confirmedBookings.length > 0 && (
           <>
             <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
               Agendamentos
             </h2>
-
-            {/* AGENDAMENTO */}
             <div className="flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
               {confirmedBookings.map((booking) => (
                 <BookingItem
@@ -97,16 +113,8 @@ const Home = async () => {
           </>
         )}
 
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Recomendados
-        </h2>
-        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
-          {barbershops.map((barbershop) => (
-            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
-          ))}
-        </div>
-
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+        {/* Populares */}
+        <h2 className="p-32  mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Populares
         </h2>
         <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
@@ -116,7 +124,7 @@ const Home = async () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
