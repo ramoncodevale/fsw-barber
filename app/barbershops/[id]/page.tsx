5ed957a2c1c7729c91b3990565ce3b40ef848fc5
ramoncodevale <ramonvalentim88@gmail.com>
2024-11-03 17:55:09 -0300

    chore: adjusting image size on desktop

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,8 +1,11 @@
+import BookingItem from "@/app/_components/booking-item"
+import BookingSummary from "@/app/_components/booking-summary"
 import Header from "@/app/_components/header"
 import PhoneItem from "@/app/_components/phone-item"
 import ServiceItem from "@/app/_components/service-item"
 import SidebarSheet from "@/app/_components/sidebar-sheet"
 import { Button } from "@/app/_components/ui/button"
+import { Card, CardContent, CardHeader } from "@/app/_components/ui/card"
 import { Sheet, SheetTrigger } from "@/app/_components/ui/sheet"
 import { db } from "@/app/_lib/prisma"
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react"
@@ -33,18 +36,26 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
 
   return (
     <div>
-      <div className="hidden lg:block"> 
+      <div className="hidden lg:block">
         <Header hasSearch={true} />
       </div>
 
+
       {/* IMAGEM */}
-      <div className="relative h-[250px] w-full lg:flex justify-center items-center">
-        <Image
-          alt={barbershop.name}
-          src={barbershop?.imageUrl}
-          fill
-          className="object-cover"
-        />
+      <div className="lg:px-32">
+        <div className="relative h-[250px] w-full lg:h-[487px] lg:w-[758px] lg:flex justify-center items-center mt-10">
+          <div className="hidden lg:flex items-center relative z-10"> {/* Adicione z-index aqui */}
+          {bookings.map((booking) => (
+      <BookingItem key={booking.id} booking={booking} />
+    ))}
+          </div>
+          <Image
+            alt={barbershop.name}
+            src={barbershop?.imageUrl}
+            fill
+            className="object-cover"
+          />
+        </div>
 
         <Button
           size="icon"
@@ -67,24 +78,36 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
               <MenuIcon />
             </Button>
           </SheetTrigger>
-        
+
           <SidebarSheet />
         </Sheet>
       </div>
 
       {/* TÍTULO */}
-      <div className="border-b border-solid p-5">
-        <h1 className="mb-3 text-xl font-bold">{barbershop.name}</h1>
-        <div className="lg:flex justify-between">
-          <div className="mb-2 flex items-center gap-2">
-            <MapPinIcon className="text-primary" size={18} />
-            <p className="text-sm">{barbershop?.address}</p>
+      <div className="border-b border-solid lg:px-32">
+        <div className="lg:flex items-center justify-between">
+          <div className="lg:flex flex-col">
+            <h1 className="mb-3 text-xl font-bold">{barbershop.name}</h1>
+            <div className="mb-2 flex items-center gap-2">
+              <MapPinIcon className="text-primary" size={18} />
+              <p className="text-sm">{barbershop?.address}</p>
+            </div>
           </div>
 
-          <div className="flex items-center gap-2">
-            <StarIcon className="fill-primary text-primary" size={18} />
-            <p className="text-sm">5,0 (499 avaliações)</p>
-          </div>
+          <Card className="lg:flex flex-col">
+            <CardHeader>
+              <div className="flex items-center gap-2">
+                <StarIcon className="fill-primary text-primary" size={18} />
+                <p className="text-sm">
+                  5,0
+                </p>
+                <span className="text-xs">(499 avaliações)</span>
+              </div>
+            </CardHeader>
+            <CardContent>
+            </CardContent>
+          </Card>
+
         </div>
       </div>
 
